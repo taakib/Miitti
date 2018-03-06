69c444670c115310b3556c4bb4ed18e620319f83
Mikael Ramo <Mikaelhr@metropolia.fi>
2018-03-06 14:23:23 +0200

    login screen improvements

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -3,6 +3,7 @@ import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {MediaProvider} from '../../providers/media/media';
 import {HttpErrorResponse} from '@angular/common/http';
 import {User} from '../../models/user';
+import {HomePage} from '../home/home';
 
 /**
  * Generated class for the RegisterPage page.
@@ -18,6 +19,8 @@ import {User} from '../../models/user';
 })
 export class RegisterPage {
 
+  login: boolean = true;
+
   user: User = {
     username: '',
     password: '',
@@ -25,7 +28,7 @@ export class RegisterPage {
   };
 
   constructor(
-    public navCtrl: NavController, public navParams: NavParams,
+    private navCtrl: NavController, public navParams: NavParams,
     public mediaProvider: MediaProvider) {
   }
 
@@ -33,6 +36,10 @@ export class RegisterPage {
     console.log('ionViewDidLoad RegisterPage');
   }
 
+ /* public createAccount() {
+    this.navCtrl.push('LoginPage');
+  }
+*/
   register() {
     console.log(this.user);
 
@@ -44,6 +51,26 @@ export class RegisterPage {
       }, (error: HttpErrorResponse) => {
         console.log(error.error.message);
       });
+  }
+
+  public doLogin(): void {
+    console.log(this.user);
+    this.mediaProvider.login(this.user).subscribe(data => {
+      console.log(data);
+      this.mediaProvider.loggedIn = true;
+      localStorage.setItem('token', data.token);
+      this.navCtrl.setRoot(HomePage);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  public checkForm () {
+    return this.login;
+  }
+
+  public changeForm() {
+    this.login = !this.login;
   }
 
 }
