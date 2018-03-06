d8b1ba8da7bff0e3a0eb8fa75294ef238b4bc1ea
kasettilameri <riikos@gmail.com>
2018-03-06 14:36:48 +0200

    upload page looks

--- src/pages/upload/upload.ts
+++ src/pages/upload/upload.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {MediaProvider} from '../../providers/media/media';
 import {HttpErrorResponse} from '@angular/common/http';
 import {Media} from '../../models/media';
+import {HomePage} from '../home/home';
 
 /**
  * Generated class for the UploadPage page.
@@ -41,7 +42,8 @@ export class UploadPage {
 
     this.mediaProvider.postUserFile(formData).subscribe(response => {
       console.log(response);
-      alert('Picture uploaded!')
+      alert('Picture uploaded!');
+      this.navCtrl.setRoot(HomePage);
     }, (error: HttpErrorResponse) => {
       console.log(error);
     });
